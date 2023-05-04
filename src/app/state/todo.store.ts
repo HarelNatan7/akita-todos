d41d02b56e7c09717d14e197367d677d56177815
Harel Natan <harelnatan7@gmail.com>
2023-05-04 22:54:16 +0300

    basic temple and store

---
+++ src/app/state/todo.store.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../todo.model';
+import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
+
+export interface TodoState extends EntityState<Todo> {}
+
+@Injectable({providedIn: 'root'})
+@StoreConfig({ name: 'todo' })
+export class TodoStore extends EntityStore<TodoState, Todo> {
+  constructor() {
+    super();
+  }
+}
